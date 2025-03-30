90fd9a5dc10d726bb4516b7274b2c5e30a35c0ee
Mirshod Mirjonov <mirshodmirjonov05@gmail.com>
2025-03-30 23:54:25 +0500

    unfinished

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,8 +1,18 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class WordNet {
+    private final String synsetsFilename;
+    private final String hypernymsFilename;
+    private final TreeSet<>
+
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
-
+        assertNonNull(synsets);
+        assertNonNull(hypernyms);
+        this.synsetsFilename = synsets;
+        this.hypernymsFilename = hypernyms;
     }
 
     // returns all WordNet nouns
@@ -12,11 +22,13 @@ public class WordNet {
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-
+        assertNonNull(word);
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
+        assertTrue(isNoun(nounA));
+        assertTrue(isNoun(nounB));
 
     }
 
@@ -24,6 +36,18 @@ public class WordNet {
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
 
+    }
+
+    private void assertNonNull(Object obj) {
+        if (obj == null) {
+            throw new IllegalArgumentException("given obj is null");
+        }
+    }
+
+    private void assertTrue(boolean value) {
+        if (!value) {
+            throw new IllegalArgumentException("value is false, when expected true");
+        }
     }
 
     // do unit testing of this class
