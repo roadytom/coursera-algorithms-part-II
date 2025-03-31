5757c887646260733674881f8a796d8175020dab
mirshod <mmirjonov@mwiah.co.uk>
2025-03-31 16:39:47 +0500

    outcast not finished

--- src/Outcast.java
+++ src/Outcast.java
@@ -2,14 +2,28 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Outcast {
+    private final WordNet wordNet;
+
     public Outcast(WordNet wordnet) {
         // constructor takes a WordNet object
-        throw new IllegalStateException("not implemented");
+        this.wordNet = wordnet;
     }
 
     public String outcast(String[] nouns) {
         // given an array of WordNet nouns, return an outcast
-        throw new IllegalStateException("not implemented");
+        int maxDistance = -1;
+        String maxDistanceNoun = null;
+        for (String noun : nouns) {
+            int distanceSum = 0;
+            for (String otherNoun : nouns) {
+                distanceSum += wordNet.distance(noun, otherNoun);
+            }
+            if (distanceSum > maxDistance) {
+                maxDistance = distanceSum;
+                maxDistanceNoun = noun;
+            }
+        }
+        return maxDistanceNoun;
     }
 
     public static void main(String[] args) {
