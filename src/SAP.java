6f2c1fa099b79f82d53b9c7e0e6399976ed53045
mirshod <mmirjonov@mwiah.co.uk>
2025-03-31 08:43:41 +0500

    some methods done

--- src/SAP.java
+++ src/SAP.java
@@ -1,4 +1,7 @@
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
 
@@ -9,26 +12,35 @@ public class SAP {
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-
+        throw new IllegalStateException("not implemented");
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-
+        throw new IllegalStateException("not implemented");
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-
+        throw new IllegalStateException("not implemented");
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-
+        throw new IllegalStateException("not implemented");
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
-
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
     }
 }
--- src/WordNet.java
+++ src/WordNet.java
@@ -1,10 +1,17 @@
-import java.util.TreeMap;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Objects;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class WordNet {
     private final String synsetsFilename;
     private final String hypernymsFilename;
-    private final TreeSet<>
+    private final TreeSet<String> nouns;
 
 
     // constructor takes the name of the two input files
@@ -13,29 +20,35 @@ public class WordNet {
         assertNonNull(hypernyms);
         this.synsetsFilename = synsets;
         this.hypernymsFilename = hypernyms;
+        In in = new In(synsetsFilename);
+        this.nouns = Stream.of(in.readAllLines())
+                .map(line -> line.split(","))
+                .map(line -> line[1])
+                .collect(Collectors.toCollection(TreeSet::new));
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-
+        return this.nouns;
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
         assertNonNull(word);
+        return this.nouns.contains(word);
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
         assertTrue(isNoun(nounA));
         assertTrue(isNoun(nounB));
-
+        throw new IllegalStateException("not implemented");
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
-
+        throw new IllegalStateException("not implemented");
     }
 
     private void assertNonNull(Object obj) {
@@ -52,6 +65,51 @@ public class WordNet {
 
     // do unit testing of this class
     public static void main(String[] args) {
+        WordNet wordNet = new WordNet(args[0], args[1]);
+        var nouns = wordNet.nouns();
+        StdOut.println(StreamSupport.stream(nouns.spliterator(), false).count());
+//        wordNet.nouns().forEach(StdOut::println);
+        while (StdIn.hasNextLine()) {
+            String noun = StdIn.readLine();
+            StdOut.printf("is noun exist in sysets? -> %s\n", wordNet.isNoun(noun));
+        }
+    }
 
+    private static class Synset {
+        private final int synsetId;
+        private final String synsetName;
+        private final String gloss;
+
+        public Synset(int synsetId, String synsetName, String gloss) {
+            this.synsetId = synsetId;
+            this.synsetName = synsetName;
+            this.gloss = gloss;
+        }
+
+        public int getSynsetId() {
+            return synsetId;
+        }
+
+        public String getSynsetName() {
+            return synsetName;
+        }
+
+
+        public String getGloss() {
+            return gloss;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Synset synset = (Synset) o;
+            return synsetId == synset.synsetId;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(synsetId);
+        }
     }
 }
