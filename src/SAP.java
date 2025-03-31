b36c83b4b49745110dc9779b11148dae56277db6
Mirshod Mirjonov <mirshodmirjonov05@gmail.com>
2025-03-31 13:45:04 +0500

    WordNet and SAP are dont

--- src/SAP.java
+++ src/SAP.java
@@ -1,33 +1,96 @@
-import edu.princeton.cs.algs4.Digraph;
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.*;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class SAP {
+    private final Digraph graph;
+
+    public Digraph getGraph() {
+        return graph;
+    }
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
-
-    }
-
-    // length of shortest ancestral path between v and w; -1 if no such path
-    public int length(int v, int w) {
-        throw new IllegalStateException("not implemented");
+        this.graph = G;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        throw new IllegalStateException("not implemented");
+        return ancestor(List.of(v), List.of(w));
+    }
+
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        return findSapResult(v, w).getSapAncestor();
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        return length(List.of(v), List.of(w));
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        throw new IllegalStateException("not implemented");
+        return findSapResult(v, w).getLengthOrMinusOne();
     }
 
-    // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        throw new IllegalStateException("not implemented");
+    private SAPResult findSapResult(Iterable<Integer> v, Iterable<Integer> w) {
+        List<Integer> vDistances = Stream.generate(() -> -1).limit(graph.V()).collect(Collectors.toList());
+        List<Integer> wDistances = Stream.generate(() -> -1).limit(graph.V()).collect(Collectors.toList());
+        Queue<Integer> vQueue = new ArrayDeque<>();
+        Queue<Integer> wQueue = new ArrayDeque<>();
+        v.forEach(x -> {
+            if (x >= graph.V() || x < 0) {
+                throw new IllegalArgumentException("x is out of range");
+            }
+            vDistances.set(x, 0);
+            vQueue.offer(x);
+        });
+        w.forEach(x -> {
+            if (x >= graph.V() || x < 0) {
+                throw new IllegalArgumentException("x is out of range");
+            }
+            wDistances.set(x, 0);
+            wQueue.offer(x);
+        });
+        SAPResult sapResult = new SAPResult(-1, Integer.MAX_VALUE);
+        var distances = List.of(vDistances, wDistances);
+        var queues = List.of(vQueue, wQueue);
+        while (!vQueue.isEmpty() || !wQueue.isEmpty()) {
+            IntStream.range(0, 2).forEach(i -> {
+                Queue<Integer> queue = queues.get(i);
+                List<Integer> distance = distances.get(i);
+                if (!queue.isEmpty()) {
+                    int poppedElement = queue.poll();
+                    if (isShorter(vDistances, wDistances, poppedElement, sapResult.getSapLength())) {
+                        sapResult.setSapLength(wDistances.get(poppedElement) + vDistances.get(poppedElement));
+                        sapResult.setSapAncestor(poppedElement);
+                    }
+                    updateNeighborDistanceAndAddQueue(queue, distance, poppedElement);
+                }
+            });
+            Stream.of(vQueue, wQueue).forEach(queue -> {
+
+            });
+        }
+        return sapResult;
+    }
+
+    private void updateNeighborDistanceAndAddQueue(Queue<Integer> queue, List<Integer> distances, int currentElement) {
+        for (int neighbor : graph.adj(currentElement)) {
+            queue.offer(neighbor);
+            distances.set(neighbor, distances.get(currentElement) + 1);
+        }
+    }
+
+    private boolean isShorter(List<Integer> vDistances, List<Integer> wDistances, int currentElement, int shortAncestorDistance) {
+        return vDistances.get(currentElement) != -1 && wDistances.get(currentElement) != -1 && wDistances.get(currentElement) + vDistances.get(currentElement) <= shortAncestorDistance;
     }
 
     // do unit testing of this class
@@ -43,4 +106,38 @@ public class SAP {
             StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
         }
     }
+
+    private static class SAPResult {
+        private int sapAncestor;
+        private int sapLength;
+
+        public SAPResult(int sapAncestor, int sapLength) {
+            this.sapAncestor = sapAncestor;
+            this.sapLength = sapLength;
+        }
+
+        public void setSapAncestor(int sapAncestor) {
+            this.sapAncestor = sapAncestor;
+        }
+
+        public int getLengthOrMinusOne() {
+            if (this.sapAncestor == -1) {
+                return -1;
+            }
+            return getSapLength();
+        }
+
+        public void setSapLength(int sapLength) {
+            this.sapLength = sapLength;
+        }
+
+        public int getSapAncestor() {
+            return sapAncestor;
+        }
+
+        public int getSapLength() {
+            return sapLength;
+        }
+    }
 }
+
--- src/WordNet.java
+++ src/WordNet.java
@@ -1,54 +1,97 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.*;
 
-import java.util.Objects;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+
 public class WordNet {
-    private final String synsetsFilename;
-    private final String hypernymsFilename;
-    private final TreeSet<String> nouns;
+    private TreeMap<String, List<Integer>> nounToIds;
+    private SAP sap;
 
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         assertNonNull(synsets);
         assertNonNull(hypernyms);
-        this.synsetsFilename = synsets;
-        this.hypernymsFilename = hypernyms;
+        initNouns(synsets);
+        initSap(hypernyms);
+    }
+
+    private void initSap(String hypernymsFilename) {
+        In in = new In(hypernymsFilename);
+        int V = nounToIds.values().stream().map(List::size).mapToInt(Integer::intValue).sum();
+        Digraph digraph = new Digraph(V);
+        Stream.of(in.readAllLines()).map(line -> line.split(",")).forEach(line -> {
+            int synsetId = Integer.parseInt(line[0]);
+            print(synsetId + " ->");
+            for (int i = 1; i < line.length; i++) {
+                int hypernymId = Integer.parseInt(line[i]);
+                print(" " + hypernymId);
+                digraph.addEdge(synsetId, hypernymId);
+            }
+            println();
+        });
+        sap = new SAP(digraph);
+    }
+
+    private void initNouns(String synsetsFilename) {
         In in = new In(synsetsFilename);
-        this.nouns = Stream.of(in.readAllLines())
-                .map(line -> line.split(","))
-                .map(line -> line[1])
-                .collect(Collectors.toCollection(TreeSet::new));
+        this.nounToIds = Stream.of(in.readAllLines()).map(line -> line.split(",")).map(Synset::new).collect(Collectors.groupingBy(Synset::getSynsetName, TreeMap::new, Collectors.mapping(Synset::getSynsetId, Collectors.toList())));
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return this.nouns;
+        return this.nounToIds.keySet();
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
         assertNonNull(word);
-        return this.nouns.contains(word);
+        return this.nounToIds.containsKey(word);
+    }
+
+    public String getNounById(int synsetId) {
+        return nounToIds.entrySet().stream()
+                .filter(entry -> entry.getValue().contains(synsetId))
+                .map(Map.Entry::getKey)
+                .findAny()
+                .orElse(null);
+    }
+
+
+    public Map<Integer, Iterable<String>> getChildren(String noun) {
+        assertTrue(isNoun(noun));
+        Map<Integer, Iterable<String>> nodeWithChildren = new HashMap<>();
+        var noundIds = nounToIds.get(noun);
+        for (Integer noundId : noundIds) {
+            var children = sap.getGraph().adj(noundId);
+            nodeWithChildren.put(noundId, StreamSupport.stream(children.spliterator(), false).map(id -> id + "-" + getNounById(id)).collect(Collectors.toUnmodifiableList()));
+        }
+        return nodeWithChildren;
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
         assertTrue(isNoun(nounA));
         assertTrue(isNoun(nounB));
-        throw new IllegalStateException("not implemented");
+        List<Integer> synsetsA = nounToIds.get(nounA);
+        List<Integer> synsetsB = nounToIds.get(nounB);
+        return sap.length(synsetsA, synsetsB);
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
-        throw new IllegalStateException("not implemented");
+        assertTrue(isNoun(nounA));
+        assertTrue(isNoun(nounB));
+        List<Integer> synsetsA = nounToIds.get(nounA);
+        List<Integer> synsetsB = nounToIds.get(nounB);
+        int synsetId = sap.ancestor(synsetsA, synsetsB);
+        return getNounById(synsetId);
     }
 
     private void assertNonNull(Object obj) {
@@ -68,12 +111,42 @@ public class WordNet {
         WordNet wordNet = new WordNet(args[0], args[1]);
         var nouns = wordNet.nouns();
         StdOut.println(StreamSupport.stream(nouns.spliterator(), false).count());
-//        wordNet.nouns().forEach(StdOut::println);
+        StdOut.println(wordNet.nounToIds.values().stream().map(List::size).mapToInt(Integer::intValue).sum());
+        StdOut.printf("Enter method name: \n");
         while (StdIn.hasNextLine()) {
-            String noun = StdIn.readLine();
-            StdOut.printf("is noun exist in sysets? -> %s\n", wordNet.isNoun(noun));
+            String option = StdIn.readLine();
+            if ("sap".equals(option)) {
+                String vNoun = StdIn.readLine();
+                String wNoun = StdIn.readLine();
+                String sapNoun = wordNet.sap(vNoun, wNoun);
+                StdOut.printf("Shortest Ancestral Noun = %s\n", sapNoun);
+            } else if ("distance".equals(option)) {
+                String vNoun = StdIn.readLine();
+                String wNoun = StdIn.readLine();
+                int sapLength = wordNet.distance(vNoun, wNoun);
+                StdOut.printf("Shortest Ancestral Length = %s\n", sapLength);
+            } else if ("isNoun".equals(option)) {
+                String noun = StdIn.readLine();
+                StdOut.printf("isNoun: %s\n", wordNet.isNoun(noun));
+            } else if ("nouns".equals(option)) {
+                StdOut.printf("Nouns length: %d\n", StreamSupport.stream(wordNet.nouns().spliterator(), false).count());
+            } else if ("children".equals(option)) {
+                String noun = StdIn.readLine();
+                wordNet.nounToIds.getOrDefault(noun, List.of())
+                        .forEach(idx -> {
+                            StdOut.printf("%d-%s -> %s\n", idx, noun, wordNet.getChildren(noun));
+                        });
+            } else {
+                throw new IllegalArgumentException("non-existing method");
+            }
+            StdOut.printf("Enter method name: \n");
         }
     }
+
+    /**
+     * happening occurrence occurrent natural_event
+     * resistance opposition
+     */
 
     private static class Synset {
         private final int synsetId;
@@ -84,6 +157,12 @@ public class WordNet {
             this.synsetId = synsetId;
             this.synsetName = synsetName;
             this.gloss = gloss;
+        }
+
+        public Synset(String[] line) {
+            this.synsetId = Integer.parseInt(line[0]);
+            this.synsetName = line[1];
+            this.gloss = line[2];
         }
 
         public int getSynsetId() {
